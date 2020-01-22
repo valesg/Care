3191c3b488c8fa337502e01e963535b5facea6c6
valesg <gvales@metcove.com>
2020-01-21 20:30:52 -0500

    Initial Commit

---
+++ Care/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Care
+//
+//  Created by Guy Vales on 2020-01-21.
+//  Copyright © 2020 Guy Vales. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
